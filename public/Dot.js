ebad99e4522633461e3b684fccf164676c37976b
Oksana Trygub <ok.trygub@gmail.com>
2022-11-03 18:08:29 +0200

    init repo

---
+++ public/Dot.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Dot = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="4" height="4" fill="currentColor"
+             className="bi bi-circle-fill inline-block"
+             viewBox="0 0 16 16">
+            <circle cx="8" cy="8" r="8"/>
+        </svg>
+    );
+};
+
+export default Dot;
